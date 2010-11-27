4e74e00237263be4403f01302a2283ed657c7daf
unknown <kyl191@.(none)>
2010-11-27 23:53:03 +0800

    Remove the 'if this archive is slow' message.

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ pw_d.projectwonderful_background_color = "";
 <p>
 <font color="00c3c3" size="7" face="Times">Two</font><font color="c77e4b" size="7" face="Times">Kinds</font> <font color="DDDDDD" size="7" face="Times">Archives</font>
 <p>
-<a href="http://2kinds.com">Home</a> | <a href="http://2kinds.com/aboutme.htm">About</a> | <a href="http://2kinds.com/archive.htm">Archives</a> | <a href="http://2kinds.com/characters.htm">Characters</a> | <a href="http://twokinds.deviantart.com">Deviantart</a> | <a href="http://2kinds.com/Fanart/index.html">Fanart</a> | <a href="http://twokinds.net/forum">Forum</a> | <a href="http://webchat.nightstar.net/?nick=Twokinds-Guest.&channels=twokinds">IRC</a> | <a href="http://2kinds.com/links_2.htm">Links</a> | <a href="mailto:twokinds@gmail.com">Contact</a><br />If this archive is slow, try <a href="http://twokindscomic.com/archive/">the alternate mirror at twokindscomic.com</a>.
+<a href="http://2kinds.com">Home</a> | <a href="http://2kinds.com/aboutme.htm">About</a> | <a href="http://2kinds.com/archive.htm">Archives</a> | <a href="http://2kinds.com/characters.htm">Characters</a> | <a href="http://twokinds.deviantart.com">Deviantart</a> | <a href="http://2kinds.com/Fanart/index.html">Fanart</a> | <a href="http://twokinds.net/forum">Forum</a> | <a href="http://webchat.nightstar.net/?nick=Twokinds-Guest.&channels=twokinds">IRC</a> | <a href="http://2kinds.com/links_2.htm">Links</a> | <a href="mailto:twokinds@gmail.com">Contact</a><br />
 <p>
 <?php include("comicgallery.php"); ?>
 <p>
