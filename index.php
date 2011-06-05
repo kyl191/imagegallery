e4f38122a45495228368c4d32326427dcaf49fb5
kyl191 <github@kyl191.net>
2011-06-05 12:20:06 +0800

    Remove a bunch of ad links in the old 2kinds-specific page

--- index.php
+++ index.php
@@ -3,24 +3,7 @@
 <title>TwoKinds Archive</title>
 <link rel="stylesheet" href="kubrick.css" type="text/css" media="screen" />
 </head>
-<body bgcolor="#011425">
 <center>
-<!-- Beginning of Project Wonderful ad code: -->
-<!-- Ad box ID: 26690 -->
-<script type="text/javascript">
-<!--
-var pw_d=document;
-pw_d.projectwonderful_adbox_id = "26690";
-pw_d.projectwonderful_adbox_type = "5";
-pw_d.projectwonderful_foreground_color = "";
-pw_d.projectwonderful_background_color = "";
-//-->
-</script>
-<script type="text/javascript" src="http://www.projectwonderful.com/ad_display.js"></script>
-<noscript><map name="admap26690" id="admap26690"><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=0&amp;id=26690&amp;type=5" shape="rect" coords="0,0,728,90" title="" alt="" target="_blank" /></map>
-<table cellpadding="0" border="0" cellspacing="0" width="733" bgcolor=""><tr><td><img src="http://www.projectwonderful.com/nojs.php?id=26690&amp;type=5" width="733" height="95" usemap="#admap26690" border="0" alt="" /></td></tr><tr><td bgcolor="" colspan="1"><center><a style="font-size:10px;color:#005ab3;text-decoration:none;line-height:1.2;font-weight:bold;font-family:Tahoma, verdana,arial,helvetica,sans-serif;text-transform: none;letter-spacing:normal;text-shadow:none;white-space:normal;word-spacing:normal;" href="http://www.projectwonderful.com/advertisehere.php?id=26690&amp;type=5" target="_blank">Ads by Project Wonderful!  Your ad could be here, right now.</a></center></td></tr></table>
-</noscript>
-<!-- End of Project Wonderful ad code. -->
 <p>
 <font color="00c3c3" size="7" face="Times">Two</font><font color="c77e4b" size="7" face="Times">Kinds</font> <font color="DDDDDD" size="7" face="Times">Archives</font>
 <p>
@@ -29,22 +12,6 @@ pw_d.projectwonderful_background_color = "";
 <?php include("imagegallery.php"); ?>
 <p>
 <br>
-<!-- Beginning of Project Wonderful ad code: -->
-<!-- Ad box ID: 26692 -->
-<script type="text/javascript">
-<!--
-var pw_d=document;
-pw_d.projectwonderful_adbox_id = "26692";
-pw_d.projectwonderful_adbox_type = "2";
-pw_d.projectwonderful_foreground_color = "";
-pw_d.projectwonderful_background_color = "";
-//-->
-</script>
-<script type="text/javascript" src="http://www.projectwonderful.com/ad_display.js"></script>
-<noscript><map name="admap26692" id="admap26692"><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=0&amp;id=26692&amp;type=2" shape="rect" coords="0,0,117,30" title="" alt="" target="_blank" /><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=1&amp;id=26692&amp;type=2" shape="rect" coords="122,0,239,30" title="" alt="" target="_blank" /><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=2&amp;id=26692&amp;type=2" shape="rect" coords="244,0,361,30" title="" alt="" target="_blank" /><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=3&amp;id=26692&amp;type=2" shape="rect" coords="366,0,483,30" title="" alt="" target="_blank" /><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=4&amp;id=26692&amp;type=2" shape="rect" coords="488,0,605,30" title="" alt="" target="_blank" /><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=5&amp;id=26692&amp;type=2" shape="rect" coords="610,0,727,30" title="" alt="" target="_blank" /></map>
-<table cellpadding="0" border="0" cellspacing="0" width="732" bgcolor=""><tr><td bgcolor="" colspan="6"><center><a style="font-size:10px;color:#005ab3;text-decoration:none;line-height:1.2;font-weight:bold;font-family:Tahoma, verdana,arial,helvetica,sans-serif;text-transform: none;letter-spacing:normal;text-shadow:none;white-space:normal;word-spacing:normal;" href="http://www.projectwonderful.com/advertisehere.php?id=26692&amp;type=2" target="_blank">Ads by Project Wonderful!  Your ad here, right now: $0</a></center></td></tr><tr><td><img src="http://www.projectwonderful.com/nojs.php?id=26692&amp;type=2" width="732" height="35" usemap="#admap26692" border="0" alt="" /></td></tr></table>
-</noscript>
-<!-- End of Project Wonderful ad code. -->
 </center>
 </body>
 </html>
