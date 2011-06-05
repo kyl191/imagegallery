8ae4ec370df13d7641839c9aecd5f31c51684320
kyl191 <github@kyl191.net>
2011-06-05 12:57:24 +0800

    Remove the last of the 2kinds-specific code

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <html>
 <head>
-<title>TwoKinds Archive</title>
-<link rel="stylesheet" href="kubrick.css" type="text/css" media="screen" />
+<title>Example Imagegallery</title>
 </head>
 <center>
 <p>
-<font color="00c3c3" size="7" face="Times">Two</font><font color="c77e4b" size="7" face="Times">Kinds</font> <font color="DDDDDD" size="7" face="Times">Archives</font>
-<p>
-<a href="http://2kinds.com">Home</a> | <a href="http://2kinds.com/aboutme.htm">About</a> | <a href="http://2kinds.com/archive.htm">Archives</a> | <a href="http://2kinds.com/characters.htm">Characters</a> | <a href="http://twokinds.deviantart.com">Deviantart</a> | <a href="http://2kinds.com/Fanart/index.html">Fanart</a> | <a href="http://twokinds.net/forum">Forum</a> | <a href="http://webchat.nightstar.net/?nick=Twokinds-Guest.&channels=twokinds">IRC</a> | <a href="http://2kinds.com/links_2.htm">Links</a> | <a href="mailto:twokinds@gmail.com">Contact</a><br />
+Your image will appear below.
+<hr size=1 noshade/>
 <p>
 <?php include("imagegallery.php"); ?>
 <p>
-<br>
+<hr size=1 noshade/>
+Your image was supposed to have appeared above.
 </center>
 </body>
 </html>
