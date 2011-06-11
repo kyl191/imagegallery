016cc0453221baf2e0585b7a5e7a4d4b2c955dd1
kyl191 <github@kyl191.net>
2011-06-11 12:30:46 +0800

    More Keenspot changes

--- archive.php
+++ archive.php
@@ -2,8 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=iso-8859-1" />
-<link rel="stylesheet" type="text/css" href="style.css" />
-<link rel="stylesheet" type="text/css" href="archive.css" />
+<link rel="stylesheet" type="text/css" href="http://cdn.twokinds.keenspot.com/style.css" />
+<link rel="stylesheet" type="text/css" href="http://cdn.twokinds.keenspot.com/archive.css" />
 <link rel="icon" type="image/x-icon" href="favicon.ico" />
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 <meta name="description" content="A webcomic about a clueless hero, a mischievous tigress, an angsty warrior, and a gender-confused wolf." />
@@ -16,7 +16,7 @@
 <title><?php echo "Archive - ".$siteTitle; ?></title>
 
 <script type="text/javascript">
-
+/* <![CDATA[ */
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-1156969-34']);
   _gaq.push(['_trackPageview']);
@@ -26,32 +26,39 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
+/* ]]> */
 </script>
 
 <!-- Quantcast Tag, part 1 -->
 <script type="text/javascript">
+/* <![CDATA[ */
 var _qevents = _qevents || [];
 (function() {
-var elem = document.createElement('
-script');
-elem.src = (document.location.protocol == "https:" ? "https://secure"
-: "http://edge") + ".quantserve.com/quant.js";
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure": "http://edge") + ".quantserve.com/quant.js";
 elem.async = true;
 elem.type = "text/javascript";
 var scpt = document.getElementsByTagName('script')[0];
 scpt.parentNode.insertBefore(elem, scpt);
 })();
+/* ]]> */
 </script>
 </head>
 <body>
 
-<script language='javascript' type='text/javascript' src='http://www.keenspot.com/ks_header.js'></script>
-<noscript><a href="http://www.keenspot.com/"><img src="http://www.keenspot.com/images/headerbar-adblockers.gif" width="519" height="32" border="0" alt="This site is supported by advertising Revenue" /></a></noscript>
+<script  type='text/javascript' src='http://www.keenspot.com/ks_header.js'></script>
+<noscript><div><a href="http://www.keenspot.com/"><img src="http://www.keenspot.com/images/headerbar-adblockers.gif" width="519" height="32"  alt="This site is supported by advertising Revenue" /></a></div></noscript>
 <!-- Header End -->
 
     <div class="archive-cont">
 			
+			<div class="arch-cont" style="width:950px;">
+			    <div class="comic">
+    			    <?php
+					    include("imagegallery.php");
+				    ?>
+				</div>
+			</div>
 		<div class="advertisement ad-top arch-topad"><script type='text/javascript'>
 <!--
 //<![CDATA[
@@ -65,56 +72,41 @@ var cwfl="http://twokinds.keenspot.com/cwfl.htm";
 		</div>
         <div class="advertisement">
 		<div class="arch-sidebar">
-		    <div class="adv adv-arch"><script type='text/javascript'>
-<!--
-//<![CDATA[
-var cwfl="http://twokinds.keenspot.com/cwfl.htm";
-// ]]>
--->
-</script><script type="text/javascript" src="http://www.keenspot.com/adp.php?&amp;type=w_sky&amp;cat=general&amp;js=1&amp;out=twokinds">
+		    <div class="adv adv-arch">
+<script type="text/javascript" src="http://www.keenspot.com/adp.php?&amp;type=w_sky&amp;cat=general&amp;js=1&amp;out=twokinds">
 </script></div>
 				    <div class="keenspot">
 					<script src="http://www.keenspot.com/ks_newsbox.js" type="text/javascript"></script>
 			</div>
 		    </div>
 	    </div>
-			<div class="arch-cont">
-			    <div class="comic">
-    			    <?php
-					    include("imagegallery.php");
-				    ?>
-				</div>
-			</div>
+
 				    
 		<div style="clear: both;"></div>
-		<div class="advertisement"><div class="adv2 adv2-archive"><script type='text/javascript'>
-<!--
-//<![CDATA[
-var cwfl="http://twokinds.keenspot.com/cwfl.htm";
-// ]]>
--->
-</script><script type="text/javascript" src="http://www.keenspot.com/adp.php?&amp;type=m_rect&amp;cat=general&amp;js=1&amp;out=twokinds">
+		<div class="advertisement"><div class="adv2 adv2-archive">
+<script type="text/javascript" src="http://www.keenspot.com/adp.php?&amp;type=m_rect&amp;cat=general&amp;js=1&amp;out=twokinds">
 </script></div></div>
 	</div>
 
 <!-- Start Quantcast tag, part 2 -->
 <script type="text/javascript">
+/* <![CDATA[ */
 _qevents.push({
 qacct:"p-0bpH4thh8w_tE"
 });
+/* ]]> */
 </script>
 
 <noscript>
 <div style="display:none;">
-<img src="//pixel.quantserve.com/pixel/p-0bpH4thh8w_tE.gif" border="0"
-height="1" width="1" alt="Quantcast"/>
+<img src="//pixel.quantserve.com/pixel/p-0bpH4thh8w_tE.gif" height="1" width="1" alt="Quantcast"/>
 </div>
 </noscript>
 <!-- End Quantcast tag -->
 
 <script type="text/javascript">
- __compete_code = 'af5c57769b6abfe853931c78981c70
-36';
+/* <![CDATA[ */
+ __compete_code = 'af5c57769b6abfe853931c78981c7036';
  (function () {
      var s = document.createElement('script'),
          e = document.getElementsByTagName('script')[0],
@@ -126,6 +118,7 @@ height="1" width="1" alt="Quantcast"/>
          s.async = true;
          if (e) { e.parentNode.insertBefore(s, e); }
      }());
+/* ]]> */
 </script>
 </body>
 </html>
